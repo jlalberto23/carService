4a0652c6d459866d097fa50209bbf983a7bebe88
crom503 <om17034@ues.edu.sv>
2022-05-22 00:09:18 -0600

    vamos

--- app/src/main/java/com/example/carservice/fragmets/ModificarUsuario.java
+++ app/src/main/java/com/example/carservice/fragmets/ModificarUsuario.java
@@ -23,10 +23,10 @@ public class ModificarUsuario extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_modificar_usuario, container, false);
-        modificar = (Button)v.findViewById(R.id.btnModificar);
-        cancelar = (Button)v.findViewById(R.id.btnCancelar);
-        user = (EditText)v.findViewById(R.id.txtUsuario);
-        pwd = (EditText)v.findViewById(R.id.txtContra);
+        modificar = (Button)v.findViewById(R.id.btnModificarU);
+        cancelar = (Button)v.findViewById(R.id.btnCancelarU1);
+        user = (EditText)v.findViewById(R.id.txtUsuario1);
+        pwd = (EditText)v.findViewById(R.id.txtContra1);
         modificar.setOnClickListener(onclick);
         cancelar.setOnClickListener(onclick);
         thiscontext = this.getActivity();
@@ -39,17 +39,18 @@ public class ModificarUsuario extends Fragment {
         public void onClick(View v) {
             switch (v.getId())
             {
-                case R.id.btnModificar:
+                case R.id.btnModificarU:
                     String resp = null;
                     mtoUsuario usuario = new mtoUsuario();
                     usuario.setUsuario(user.getText().toString());
                     usuario.setContra(pwd.getText().toString());
                     cn.abrir();
-                    //resp=cn.actualizar(usuario);
+                    resp=cn.actualizar(usuario);
                     cn.cerrar();
                     Toast.makeText(v.getContext(), resp, Toast.LENGTH_SHORT).show();
+                    limpiarTxt();
                     break;
-                case R.id.btnCancelar:
+                case R.id.btnCancelarU1:
                     limpiarTxt();
                     break;
             }
