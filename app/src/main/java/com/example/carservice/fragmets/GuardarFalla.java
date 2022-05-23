07999ba02453d71b03ebb34afcda631113fb117a
crom503 <om17034@ues.edu.sv>
2022-05-22 21:03:21 -0600

    ds

--- app/src/main/java/com/example/carservice/fragmets/GuardarFalla.java
+++ app/src/main/java/com/example/carservice/fragmets/GuardarFalla.java
@@ -1,66 +1,68 @@
 package com.example.carservice.fragmets;
-
+import android.content.Context;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.carservice.R;
+import com.example.carservice.mto.Conexion;
+import com.example.carservice.mto.mtoCliente;
+import com.example.carservice.mto.mtoFalla;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GuardarFalla#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class GuardarFalla extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public GuardarFalla() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GuardarFalla.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GuardarFalla newInstance(String param1, String param2) {
-        GuardarFalla fragment = new GuardarFalla();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Conexion cn;
+    Button guardar,cancelar;
+    EditText codigo,descripcion,cat;
+    Context thiscontext = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_guardar_falla, container, false);
+        View v = inflater.inflate(R.layout.fragment_guardar_falla, container, false);
+        thiscontext = this.getActivity();
+        cn = new Conexion(thiscontext);
+        guardar = (Button) v.findViewById(R.id.btnGuardarFalla);
+        cancelar = (Button) v.findViewById(R.id.btnCancelarFG);
+        codigo = (EditText) v.findViewById(R.id.txtCodigoFallaG);
+        descripcion = (EditText) v.findViewById(R.id.txtDescripcion);
+        cat = (EditText) v.findViewById(R.id.txtCodCatFG);
+
+        return v;
+    }
+
+    View.OnClickListener onclick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId())
+            {
+                case R.id.btnGuardarFalla:
+                    String resp;
+                    mtoFalla falla = new mtoFalla();
+                    falla.setId_falla(Integer.parseInt(codigo.getText().toString()));
+                    falla.setDescripcion(descripcion.getText().toString());
+                    falla.setId_categoria_falla(Integer.parseInt(cat.getText().toString()));
+                    cn.abrir();
+                    resp=cn.insertar(falla);
+                    cn.cerrar();
+                    Toast.makeText(v.getContext(), resp, Toast.LENGTH_SHORT).show();
+                    limpiarTxt();
+                    break;
+                case R.id.btnCancelarFG:
+                    limpiarTxt();
+                    break;
+            }
+        }
+    };
+
+    private void limpiarTxt(){
+        codigo.setText("");
+        descripcion.setText("");
+        cat.setText("");
     }
 }
--- app/src/main/java/com/example/carservice/fragmets/ModificarCliente.java
+++ app/src/main/java/com/example/carservice/fragmets/ModificarCliente.java
@@ -1,66 +1,90 @@
 package com.example.carservice.fragmets;
-
+import android.content.Context;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 import com.example.carservice.R;
+import com.example.carservice.mto.Conexion;
+import com.example.carservice.mto.mtoCliente;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ModificarCliente#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ModificarCliente extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ModificarCliente() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ModificarCliente.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ModificarCliente newInstance(String param1, String param2) {
-        ModificarCliente fragment = new ModificarCliente();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    Conexion cn;
+    Button modificar,cancelar;
+    EditText codigo,usuario,nombre,apellido,telefono,direccion,dui;
+    TextView fecha_ingreso;
+    Context thiscontext = null;
+    long ahora = System.currentTimeMillis();
+    Date fecha = new Date(ahora);
+    DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+    String salida = df.format(fecha);
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_modificar_cliente, container, false);
+        View v = inflater.inflate(R.layout.fragment_modificar_cliente, container, false);
+        modificar = (Button) v.findViewById(R.id.btnModificarCli);
+        cancelar = (Button) v.findViewById(R.id.btnCancelarClienM);
+        codigo = (EditText)v.findViewById(R.id.editTextTextPassword);
+        usuario = (EditText) v.findViewById(R.id.txtUsuario3);
+        nombre = (EditText) v.findViewById(R.id.editTextTextPersonName3);
+        apellido = (EditText) v.findViewById(R.id.editTextTextPersonName30);
+        telefono = (EditText) v.findViewById(R.id.editTextTextPersonName32);
+        direccion = (EditText) v.findViewById(R.id.editTextTextPersonName5);
+        dui = (EditText) v.findViewById(R.id.editTextTextPersonName6);
+        modificar.setOnClickListener(onclick);
+        cancelar.setOnClickListener(onclick);
+        thiscontext = this.getActivity();
+        cn = new Conexion(thiscontext);
+        return v;
     }
+
+    View.OnClickListener onclick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.btnModificarCli:
+                    String resp;
+                    mtoCliente cliente = new mtoCliente();
+                    cliente.setId_cliente(Integer.parseInt(codigo.getText().toString()));
+                    cliente.setNombre(nombre.getText().toString());
+                    cliente.setApellidos(apellido.getText().toString());
+                    cliente.setUsuario(usuario.getText().toString());
+                    cliente.setFecha_registro(salida);
+                    cliente.setTelefono(telefono.getText().toString());
+                    cliente.setDireccion(direccion.getText().toString());
+                    cliente.setDui(dui.getText().toString());
+                    cn.abrir();
+                    resp=cn.actualizar(cliente);
+                    cn.cerrar();
+                    Toast.makeText(v.getContext(), resp, Toast.LENGTH_SHORT).show();
+                    limpiarTxt();
+                    break;
+                case R.id.btnCancelarClienM:
+                    limpiarTxt();
+                    break;
+            }
+        }
+
+    };
+
+    private void limpiarTxt(){
+        codigo.setText("");
+        usuario.setText("");
+        nombre.setText("");
+        apellido.setText("");
+        telefono.setText("");
+        direccion.setText("");
+        dui.setText("");
+    }
+
 }
